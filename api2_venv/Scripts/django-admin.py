baeda46501b7af009a5143cbd844796146f7105b
withHappy <hap2.dev@gmail.com>
2020-06-11 01:18:59 +0900

    add gitignore

--- api2_venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\zeny\documents\dev\tutorial-api2\api2_venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
